c305f6ed763923857564cf0f11bc691a618a50b5
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-08-01 20:53:09 -0400

    passed tests

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,25 @@
 package zipcode;
 
 public class Hamming {
+    String s;
+    String s1;
     public Hamming(String s, String s1) {
+        this.s = s;
+        this.s1 = s1;
+        if (s.length() < s1.length() || s.length() > s1.length()){
+                throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
+
     public int getHammingDistance() {
-        return -1;
+        int i = 0, count = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) != s1.charAt(i)){
+                count++;
+            }
+            i++;
+        }
+        return count;
     }
 }
